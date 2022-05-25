1e97b47ea534a75f2d544ca53e39f2dcca612ea2
Corinne Riddell <corinneriddell@biostat-mbp14-02.dyn.berkeley.edu>
2022-05-25 14:53:26 -0700

    updated simulation plots

--- plot-simulation-results.R
+++ plot-simulation-results.R
@@ -4,7 +4,7 @@ library(magrittr)
 library(forcats)
 library(patchwork)
 
-results_df_summary <- read_csv("./results/twfe_sim_results_summary_PTB_n1000_05022022.csv")
+results_df_summary <- read_csv("./results/twfe_sim_results_summary_PTB_n1000_05242022.csv")
 
 table(results_df_summary$parameter)
 #CTE, many DTE and HTE
